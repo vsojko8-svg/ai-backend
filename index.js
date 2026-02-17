d0c05208ac796b57c9541d08b5e36232363589c9
vsojko8-svg <vsojko8@gmail.com>
2026-02-17 17:21:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ app.post("/api/chat", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("AI backend работает 🚀");
 });
-app.post("api/chat", async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
